70c58ccdf27057703b1a3a88f77802d20956dd05
soeyeon <57591478+soeyeon@users.noreply.github.com>
2021-06-09 09:27:22 +0900

    Add files via upload

--- js/mbtim.js
+++ js/mbtim.js
@@ -1,6 +1,6 @@
-var W   =  515; //각 이미지의 넓이
-var go  =   -1; //어느 방향으로 갈 것인지 저장.(-1 : 왼쪽방향 1 pixel 이동, +1 : 오른쪽 방향 1 pixel 이동)
-var stop;       //스크롤을 중지하기 위한 setTimeout 의 id 값을 저장하기 위한 변수.
+var W   =  515;
+var go  =   -1;
+var stop;       
 var ctnt=new Array( "<a href=# onclick='alert(1)'><img src=img/enfj.jpg></a>"
 , "<a href=# onclick='alert(2)'><img src=img/enfp.jpg></a>"
 , "<a href=# onclick='alert(3)'><img src=img/entj.jpg></a>"
@@ -17,10 +17,10 @@ var ctnt=new Array( "<a href=# onclick='alert(1)'><img src=img/enfj.jpg></a>"
 , "<a href=# onclick='alert(14)'><img src=img/isfp.jpg></a>"
 , "<a href=# onclick='alert(15)'><img src=img/istj.jpg></a>"
 , "<a href=# onclick='alert(16)'><img src=img/istp.jpg></a>");
-var stop_time   = 1000;    // 하나의 아이템이 돌아가고 잠시 멈칫 하는 시간 (1초 = 1000)
-var sspeed      =   10;    // 아이템이 움직이는 시간
+var stop_time   = 1000;    
+var sspeed      =   10;    
 function scroll(){
-clearTimeout(stop); // 일시정지,재생
+clearTimeout(stop);
 var isStop = false;
 for(var i=0;i<ctnt.length;i++){
 var temp=document.getElementById("slider_"+i);
@@ -50,7 +50,7 @@ scroll();
 }
 
 
-// 왼쪽(n=-1) 오른쪽(n=1) 한칸씩 이동
+
 function scrollMove(n){
 for(var i=0;i<ctnt.length;i++){
 var temp=document.getElementById("slider_"+i);
@@ -59,7 +59,7 @@ if(n < 0){
 temp.style.left= (max+parseInt(temp.style.left))%max-W;
 }
 else if(n > 0){
-// alert((parseInt(temp.style.left))%max+W);
+
 temp.style.left= W*ctnt.length == (parseInt(temp.style.left))%max+W ? 0 : (parseInt(temp.style.left))%max+W;
 }
 }
--- js/weather.js
+++ js/weather.js
@@ -2,9 +2,6 @@ const weather = document.querySelector(".js-weather");
 const API_KEY = "38cb84355b7ad3bb3676b3b9e6b11d39";
 const COORDS = "coords";
 
-// 여기서 fetch 함수는 requests 함수와 같은 역할.
-// then은 데이터가 완전히 넘어왔을 때에만 설정된 함수를 호출함.
-// 만약 여기서 then을 쓰지 않으면, 아직 API 데이터가 넘어오지 않았는데, 그 다음 함수가 실행되어서 에러가 날 수도 있음!!
 function getWeather(lat, lng){
     fetch(
         `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
@@ -21,7 +18,7 @@ function saveCoords(coordsObj){
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
 }
 
-// 위치정보 획득에 성공하면 실행하는 함수
+
 function handleGeoSuccess(position){
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
@@ -33,16 +30,15 @@ function handleGeoSuccess(position){
     getWeather(latitude, longitude);
 }
 
-// 위치정보 획득에 실패하면 실행하는 함수.
+
 function handleGeoError(){
     console.log("Can't access geo location");
 }
 
 function askForCoords(){
-    navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError); // 유저의 현재 위치 정보를 요청하고, 성공하면 첫 번째 함수를, 실패하면 두 번째 함수를 실행.
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError); 
 }
 
-// 로컬 스토리지에 정보가 없으면 요청.
 function loadCoords(){
     const loadedCoords = localStorage.getItem(COORDS); 
     if(loadedCoords === null){
